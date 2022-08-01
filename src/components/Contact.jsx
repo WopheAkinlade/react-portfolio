3603d3c1074249bfe38d36160de9fad895a0158d
Wophe <wophe.akinlade@gmail.com>
2022-08-01 13:28:25 +0100

    Added new components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default Contact
---
+++ src/components/Stack.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from "@mui/material";
+
+const Stack = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default Stack
