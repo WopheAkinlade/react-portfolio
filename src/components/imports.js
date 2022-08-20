6ffb1f350ffd241ed66f5c95d5df8c3bc95ece72
Wophe <wophe.akinlade@gmail.com>
2022-08-20 16:42:29 +0100

    Created icons object

---
+++ src/components/imports.js
@@ -0,0 +1,21 @@
+import html5 from '@iconify/icons-logos/html-5';
+import css3 from '@iconify/icons-logos/css-3';
+import fileTypeJsOfficial from '@iconify/icons-vscode-icons/file-type-js-official';
+import reactIcon from '@iconify/icons-logos/react';
+import materialUi from '@iconify/icons-logos/material-ui';
+import javaIcon from '@iconify/icons-logos/java';
+import pythonIcon from '@iconify/icons-logos/python';
+
+
+const icons = {
+    html5,
+    css3,
+    fileTypeJsOfficial,
+    reactIcon,
+    materialUi,
+    javaIcon,
+    pythonIcon,
+}
+
+
+export default icons
